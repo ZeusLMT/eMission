7a345ffaeb61c6632c9cd759610d811a555a5c32
Tuan Le <tuan.le@futuremark.com>
2019-10-17 13:21:22 +0300

    Create util func to add gradient color to text

---
+++ app/src/main/java/com/wildcard/eMission/Utils.kt
@@ -0,0 +1,25 @@
+package com.wildcard.eMission
+
+import android.graphics.LinearGradient
+import android.graphics.Shader
+import android.widget.TextView
+import timber.log.Timber
+
+class Utils {
+    companion object {
+
+        fun setGradientTextColor(textView: TextView, colorStart: Int, colorEnd: Int) {
+            textView.setTextColor(colorEnd)
+            val x1 = getTextSize(textView)
+            Timber.d("$x1")
+            val gradientShader =
+                LinearGradient(0f, 0f, x1, 0f, colorStart, colorEnd, Shader.TileMode.MIRROR)
+            textView.paint.shader = gradientShader
+        }
+
+
+        private fun getTextSize(textView: TextView): Float {
+            return textView.paint.measureText(textView.text.toString())
+        }
+    }
+}
--- app/src/main/java/com/wildcard/eMission/ui/home/HomeFragment.kt
+++ app/src/main/java/com/wildcard/eMission/ui/home/HomeFragment.kt
@@ -4,28 +4,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.wildcard.eMission.R
+import com.wildcard.eMission.Utils
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
-
-    private lateinit var homeViewModel: HomeViewModel
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-            ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
-        return root
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        text_home.text = "This is home Fragment"
+        Utils.setGradientTextColor(
+            text_home,
+            context!!.getColor(R.color.colorPrimary_blue),
+            context!!.getColor(R.color.colorPrimary_red)
+        )
     }
 }

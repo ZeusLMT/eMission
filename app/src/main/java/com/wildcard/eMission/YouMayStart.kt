9615530b21c311fbf9f9a13f5ce5e777b9ceb63c
juhavuo <juha.vuokko@pp.inet.fi>
2019-11-22 12:26:29 +0200

    background gradients changed in starting questions part

---
+++ app/src/main/java/com/wildcard/eMission/YouMayStart.kt
@@ -0,0 +1,12 @@
+package com.wildcard.eMission
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class YouMayStart : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_you_may_start)
+    }
+}

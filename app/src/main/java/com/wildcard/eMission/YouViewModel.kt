fe98e23f6079f45d6be9dc53e9c8d1c7e94ca802
Tuan Le <lemanhtuan275@gmail.com>
2019-12-03 00:01:54 +0200

    Revert "Development: you tab (#6)"
    This reverts commit 49cfa7e7dd1e14f9d3ca4a946d6d90c0c52addad.

--- app/src/main/java/com/wildcard/eMission/YouViewModel.kt
+++ app/src/main/java/com/wildcard/eMission/YouViewModel.kt
@@ -1,13 +1,13 @@
-package com.wildcard.eMission.ui.learning
+package com.wildcard.eMission
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class LearningViewModel : ViewModel() {
+class YouViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is you Fragment"
     }
     val text: LiveData<String> = _text
 }
--- app/src/main/java/com/wildcard/eMission/ui/learning/LearningFragment.kt
+++ app/src/main/java/com/wildcard/eMission/ui/learning/LearningFragment.kt
@@ -4,15 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.wildcard.eMission.R
-import com.wildcard.eMission.Utils
 
 class LearningFragment : Fragment() {
 
@@ -31,31 +27,5 @@ class LearningFragment : Fragment() {
             textView.text = it
         })
         return root
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        setupActionBar()
-        activity?.findViewById<BottomNavigationView>(R.id.nav_view)?.itemTextColor = context?.getColorStateList(R.color.nav_item_color_state_list_3)
-        activity?.findViewById<BottomNavigationView>(R.id.nav_view)?.itemIconTintList = context?.getColorStateList(R.color.nav_item_color_state_list_3)
-    }
-
-    private fun setupActionBar() {
-        val actionBar = activity?.findViewById<Toolbar>(R.id.toolbar)
-
-        val actionBarTitle = actionBar?.findViewById<TextView>(R.id.actionbar_title)
-        val actionBarSubtitle = actionBar?.findViewById<TextView>(R.id.actionbar_subtitle)
-        actionBarTitle?.text = getString(R.string.title_learning)
-        actionBarSubtitle?.text = getString(R.string.subtitle_learning)
-
-        Utils.setGradientTextColor(
-            actionBarTitle!!,
-            context!!.getColor(R.color.colorPrimary_green),
-            context!!.getColor(R.color.colorPrimary_yellow)
-        )
-
-        val optionalLayout = actionBar.findViewById<LinearLayout>(R.id.optional_layout)
-        optionalLayout.removeAllViews()
     }
 }

ba3c96ef41781f09eb5898fd7e1e5ab155131d34
Tuan Le <lemanhtuan275@gmail.com>
2019-12-03 00:02:23 +0200

    Revert "Development: you tab (#6)" (#7)
    This reverts commit 49cfa7e7dd1e14f9d3ca4a946d6d90c0c52addad.

--- app/src/main/java/com/wildcard/eMission/Utils.kt
+++ app/src/main/java/com/wildcard/eMission/Utils.kt
@@ -1,16 +1,12 @@
 package com.wildcard.eMission
 
-import android.graphics.*
+import android.graphics.LinearGradient
+import android.graphics.Shader
 import android.widget.TextView
-import com.squareup.picasso.Transformation
 import timber.log.Timber
-
 
 class Utils {
     companion object {
-        const val PREF_DATE = "LAST_GENERATED_DATE"
-        const val PREF_USER = "CURRENT_USER"
-        const val PREF_THEME = "CURRENT_THEME"
 
         fun setGradientTextColor(textView: TextView, colorStart: Int, colorEnd: Int) {
             textView.setTextColor(colorEnd)
@@ -24,35 +20,6 @@ class Utils {
 
         private fun getTextSize(textView: TextView): Float {
             return textView.paint.measureText(textView.text.toString())
-        }
-
-        class PicassoCircleTransformation : Transformation {
-            override fun transform(source: Bitmap): Bitmap {
-                val size = Math.min(source.width, source.height)
-                val x = (source.width - size) / 2
-                val y = (source.height - size) / 2
-                val squaredBitmap = Bitmap.createBitmap(source, x, y, size, size)
-                if (squaredBitmap != source) {
-                    source.recycle()
-                }
-                val bitmap = Bitmap.createBitmap(size, size, source.config)
-                val canvas = Canvas(bitmap)
-                val paint = Paint()
-                val shader = BitmapShader(
-                    squaredBitmap,
-                    Shader.TileMode.CLAMP, Shader.TileMode.CLAMP
-                )
-                paint.shader = shader
-                paint.isAntiAlias = true
-                val r = size / 2f
-                canvas.drawCircle(r, r, r, paint)
-                squaredBitmap.recycle()
-                return bitmap
-            }
-
-            override fun key(): String {
-                return "circle"
-            }
         }
     }
 }
--- app/src/main/java/com/wildcard/eMission/ui/you/YouViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.wildcard.eMission.ui.you
-
-import androidx.lifecycle.ViewModel
-
-class YouViewModel : ViewModel() {
-
-}

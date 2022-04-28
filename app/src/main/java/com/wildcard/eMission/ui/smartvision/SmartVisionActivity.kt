a5936d0aa2092fde0bec695181b1c64a9c5de1ad
Tuan Le <tuan.le@futuremark.com>
2022-04-28 17:38:41 +0300

    Move camera control button and result text to a bottom controller area, add NNAPI switch. Make fullscreen

--- app/src/main/java/com/wildcard/eMission/ui/smartvision/SmartVisionActivity.kt
+++ app/src/main/java/com/wildcard/eMission/ui/smartvision/SmartVisionActivity.kt
@@ -22,6 +22,10 @@ import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.google.android.gms.tasks.Task
 import com.google.android.gms.tflite.java.TfLite
 import com.wildcard.eMission.databinding.ActivitySmartVisionBinding
@@ -37,7 +41,6 @@ import kotlin.math.log
 class SmartVisionActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySmartVisionBinding
     private lateinit var fullscreenContentLayout: ConstraintLayout
-    private val hideHandler = Handler()
 
     // Camera activity related variables
     private lateinit var cameraExecutor: ExecutorService
@@ -50,35 +53,6 @@ class SmartVisionActivity : AppCompatActivity() {
     private var classifier: ImageClassifier? = null
 //    private var classifier: ImageClassificationHelper? = null
 
-    @SuppressLint("InlinedApi")
-    private val hidePart2Runnable = Runnable {
-        fullscreenContentLayout.windowInsetsController?.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
-    }
-    private val showPart2Runnable = Runnable {
-        // Delayed display of UI elements
-        supportActionBar?.show()
-    }
-    private var isFullscreen: Boolean = false
-
-    private val hideRunnable = Runnable { hide() }
-
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    private val delayHideTouchListener = View.OnTouchListener { view, motionEvent ->
-        when (motionEvent.action) {
-            MotionEvent.ACTION_DOWN -> if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS)
-            }
-            MotionEvent.ACTION_UP -> view.performClick()
-            else -> {
-            }
-        }
-        false
-    }
-
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -88,7 +62,14 @@ class SmartVisionActivity : AppCompatActivity() {
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        isFullscreen = true
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        val windowInsetsController =
+            ViewCompat.getWindowInsetsController(window.decorView) ?: return
+        // Configure the behavior of the hidden system bars
+        windowInsetsController.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        // Hide both the status bar and the navigation bar
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
 
         Log.d("CameraX","OnCreate")
 
@@ -111,15 +92,7 @@ class SmartVisionActivity : AppCompatActivity() {
                 this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
         }
 
-        // Set up the user interaction to manually show or hide the system UI.
-        fullscreenContentLayout = binding.fullscreenContentLayout
-        fullscreenContentLayout.setOnClickListener { toggle() }
-
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        binding.dummyButton.setOnTouchListener(delayHideTouchListener)
-        binding.dummyButton.setOnClickListener {
+        binding.closeButton.setOnClickListener {
             finish()
         }
 
@@ -144,62 +117,10 @@ class SmartVisionActivity : AppCompatActivity() {
         }
     }
 
-    override fun onPostCreate(savedInstanceState: Bundle?) {
-        super.onPostCreate(savedInstanceState)
-
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
-        delayedHide(100)
-    }
-
     override fun onDestroy() {
         super.onDestroy()
         cameraExecutor.shutdown()
         classifier?.close()
-    }
-
-    private fun toggle() {
-        if (isFullscreen) {
-            hide()
-        } else {
-            show()
-        }
-    }
-
-    private fun hide() {
-        // Hide UI first
-        supportActionBar?.hide()
-        isFullscreen = false
-
-        // Schedule a runnable to remove the status and navigation bar after a delay
-        hideHandler.removeCallbacks(showPart2Runnable)
-        hideHandler.postDelayed(hidePart2Runnable, UI_ANIMATION_DELAY.toLong())
-    }
-
-    private fun show() {
-        // Show the system bar
-        if (Build.VERSION.SDK_INT >= 30) {
-            fullscreenContentLayout.windowInsetsController?.show(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
-        } else {
-            fullscreenContentLayout.systemUiVisibility =
-                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-        }
-        isFullscreen = true
-
-        // Schedule a runnable to display UI elements after a delay
-        hideHandler.removeCallbacks(hidePart2Runnable)
-        hideHandler.postDelayed(showPart2Runnable, UI_ANIMATION_DELAY.toLong())
-    }
-
-    /**
-     * Schedules a call to hide() in [delayMillis], canceling any
-     * previously scheduled calls.
-     */
-    private fun delayedHide(delayMillis: Int) {
-        hideHandler.removeCallbacks(hideRunnable)
-        hideHandler.postDelayed(hideRunnable, delayMillis.toLong())
     }
 
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
@@ -339,7 +260,7 @@ class SmartVisionActivity : AppCompatActivity() {
             // Update the text and UI
             binding.textPredictions.text =
                 recognitions.subList(0, NUM_OF_RESULTS).joinToString(separator = "\n") {
-                    "${"%.2f".format(it.accuracy)} ${it.title}"
+                    "${"%.1f".format(it.accuracy*100)}% ${it.title}"
                 }
 
             // Make sure all UI elements are visible
